6e1db1106fccba6f277f66c5bbd261e5e7d8dadc
Adrian Braga <adrian.braga.ro@gmail.com>
2022-04-28 08:26:33 -0400

    Page home finished

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
-const express = require("express")
+const express = require("express");
+const nunjucks = require('nunjucks');
 
 const app = express()
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./public'))
+
+app.set('view engine', '.njk');
+
+nunjucks.configure('./src/app/views', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
+
 app.get("/", (req, res) => {
-  return res.json({ message: "Rota funcionando!" })
+  return res.render('pages/home')
 })
 
 app.listen(5000, () => console.log("Servidor rodando na porta 5000"))
